9f7583d4e5713fe872c888ccc64b4ee5c02f5347
Adithyacnair <adithyaapps143@gmail.com>
2024-04-10 11:15:29 +0530

    CRUD With Razor Pages

---
+++ Pages/ProductMaster/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CRUDWithRazorPages.DAL;
+using CRUDWithRazorPages.Models;
+
+namespace CRUDWithRazorPages.Pages.ProductMaster
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly CRUDWithRazorPages.DAL.AppDbContext _context;
+
+        public DetailsModel(CRUDWithRazorPages.DAL.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Product Product { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                Product = product;
+            }
+            return Page();
+        }
+    }
+}
---
+++ Pages/ProductMaster/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using CRUDWithRazorPages.DAL;
+using CRUDWithRazorPages.Models;
+
+namespace CRUDWithRazorPages.Pages.ProductMaster
+{
+    public class IndexModel : PageModel
+    {
+        private readonly CRUDWithRazorPages.DAL.AppDbContext _context;
+
+        public IndexModel(CRUDWithRazorPages.DAL.AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Product = await _context.Products.ToListAsync();
+        }
+    }
+}
